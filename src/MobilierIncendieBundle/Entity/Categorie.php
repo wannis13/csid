e267d356cc8254293cdf28ca608af46231aa109e
wannis13 <wannis13@gmail.com>
2016-12-13 09:02:45 +0100

    test

---
+++ src/MobilierIncendieBundle/Entity/Categorie.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This file is part of the CSID project.
+ *
+ * (c) Barbara
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace MobilierIncendieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Categorie
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Categorie
+{	
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+	
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="name", type="string", length=50)
+	 */
+	private $name;
+
+	/**
+	 *
+	 * @ORM\ManyToMany(targetEntity="MobilierIncendieBundle\Entity\Produits", mappedBy="categories")
+	 *
+	 */
+	private $produits;
+
+    public function __construct() {
+        $this->produits = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+
+	public  function __toString()
+	{
+		return $this->getName();
+	}
+
+    /**
+     * @return mixed
+     */
+    public function getProduits()
+    {
+        return $this->produits;
+    }
+
+    /**
+     * @param mixed $produits
+     */
+    public function setProduits($produits)
+    {
+        $this->produits = $produits;
+    }
+
+
+}
